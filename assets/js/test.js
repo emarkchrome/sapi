d7c8ce0aa359e1ef7320cfb4c5778e87f0e86818
Emmanuel Mark <info@emmanuelmark.guru>
2018-06-12 21:41:34 -0400

    Add stopwatch JS class and implement

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,3 +1,30 @@
+class Stopwatch {
+  constructor() {
+    this.time = 0;
+  }
+  start() {
+    var stopwatch = this;
+    this.interval = setInterval(function() {
+      stopwatch.time = stopwatch.time + 1000;
+      console.log(stopwatch.time/1000 + 's');
+    }, 1000);
+  }
+  stop() {
+    clearInterval(this.interval);
+    console.log('final result: ' + this.time/1000 + 's')
+  }
+  reset() {
+    this.time = 0;
+    document.getElementById('num').innerHTML = '0s';
+    console.info('Stopwatch Reset!');
+  }
+	getTimeInSeconds() {
+		return this.time/1000;
+	}
+}
+
+var stopwatch = new Stopwatch();
+
 $(document).ready(function() {
 
 	$('body').fadeIn();
@@ -8,10 +35,18 @@ $(document).ready(function() {
 		instructions.style.transform = 'translateY(-100vh)';
 		test.style.transform = 'translateY(0)';
 
+		stopwatch.start();
+
 		$('.stoptest').click(function() {
+			stopwatch.stop();
+
+			var readingSpeedInWordsPerMinute = Math.round(200 / (stopwatch.getTimeInSeconds() / 60) + 3);
+			console.log(`[Sapi] Reading speed is ${readingSpeedInWordsPerMinute}wpm.`)
+			var readingSpeedText = document.getElementsByClassName('reading-speed')[0];
+			readingSpeedText.innerHTML = readingSpeedInWordsPerMinute;
 			var modal = document.getElementsByClassName('modal')[0];
 			var stopButton = document.getElementsByClassName('stoptest')[0];
-			//modal.classList.add('open');
+			modal.classList.add('open');
 
 			$('.letsgo').click(function() {
 				$('body').fadeOut(function() {
