852cbc19fe159883a43de570c19a6b1b195403b1
Emmanuel Mark <info@emmanuelmark.guru>
2018-06-13 11:31:11 -0400

    Write content script to get words in article

---
+++ assets/js/content.js
@@ -0,0 +1,10 @@
+chrome.storage.sync.get(null, function(data) {
+	if (document.body.getElementsByTagName('article')[0]) {
+		var articleElement = document.body.getElementsByTagName('article')[0];
+		var wordsInArticle = articleElement.innerText.split(' ').length;
+		console.log(wordsInArticle + ' words in article approximately');
+		chrome.runtime.sendMessage({ action: 'sendwordsinarticle', words: wordsInArticle });
+	} else {
+		chrome.runtime.sendMessage({ action: 'noarticle' });
+	}
+});
