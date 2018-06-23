51dc625907136498a4700bef945b2e2192a75c0f
Emmanuel Mark <info@emmanuelmark.guru>
2018-06-23 19:18:08 -0400

    Added Sapi Reset button logic and other fixes

--- assets/js/options.js
+++ assets/js/options.js
@@ -2,38 +2,51 @@ $(document).ready(function() {
 
 	var previousFormValue;
 
-	$('body').fadeIn();
+	chrome.storage.sync.get(function(data) {
 
-	$('#reading-speed-form-range').on('input', function() {
 		var readingSpeedText = document.getElementById('reading-speed-form-text');
-		readingSpeedText.value = this.value;
-	});
+		var readingSpeedRange = document.getElementById('reading-speed-form-range');
+		readingSpeedText.value = data.readingSpeed;
+		readingSpeedRange.value = data.readingSpeed;
 
-	$('#reading-speed-form-range').change(function() {
-		chrome.storage.sync.set({ readingSpeed: this.value });
-	});
+		$('body').fadeIn();
 
-	$('#reading-speed-form-text').focusin(function(event) {
-		this.select();
-		previousFormValue = this.value;
-	});
+		$('#reading-speed-form-range').on('input', function() {
+			readingSpeedText.value = this.value;
+		});
 
-	$('#reading-speed-form-text').keypress(function(event) {
-		if (event.code == 'Enter') {
-			this.blur();
-		} else if (event.code.indexOf('Digit') == -1) {
-			event.preventDefault();
-		}
-	});
-
-	$('#reading-speed-form-text').focusout(function() {
-		if (this.value == '') {
-			this.value = previousFormValue;
-		} else {
-			var readingSpeedRange = document.getElementById('reading-speed-form-range');
-			readingSpeedRange.value = parseInt(this.value);
+		$('#reading-speed-form-range').change(function() {
 			chrome.storage.sync.set({ readingSpeed: this.value });
-		}
+		});
+
+		$('#reading-speed-form-text').focusin(function(event) {
+			this.select();
+			previousFormValue = this.value;
+		});
+
+		$('#reading-speed-form-text').keypress(function(event) {
+			if (event.code == 'Enter') {
+				this.blur();
+			} else if (event.code.indexOf('Digit') == -1) {
+				event.preventDefault();
+			}
+		});
+
+		$('#reading-speed-form-text').focusout(function() {
+			if (this.value == '') {
+				this.value = previousFormValue;
+			} else {
+
+				readingSpeedRange.value = parseInt(this.value);
+				chrome.storage.sync.set({ readingSpeed: this.value });
+			}
+		});
+
+		$('#reset-sapi').click(function() {
+			chrome.storage.sync.clear();
+			window.location.href='test.html';
+		});
+
 	});
 
 });
