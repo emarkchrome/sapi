85c5359c38c3c741dbc4ea13f7388a9b1e62dd8f
Emmanuel Mark <info@emmanuelmark.guru>
2018-06-24 18:09:11 -0400

    Redirect added
    Redirect user to onboarding if needed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,9 @@ $(document).ready(function() {
 	}
 	*/
 	chrome.storage.sync.get(function(data) { //
+		if(data.userNeedsOnboarding == true) {
+			window.location.href = chrome.runtime.getURL('onboarding.html');
+		}
 		var wordsInArticleText = document.getElementById('words-in-article');
 		var minutesToRead = document.getElementById('minutes-to-read');
 		wordsInArticleText.innerText = data.wordsInArticle;
